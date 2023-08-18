7f2e0705f82b2ca25983d30772268447a2b8773f
dheemanth <deemanthdb18@gmail.com>
2023-08-18 14:33:34 +0530

    Multiplication added

---
+++ Python-progs/arith.py
@@ -0,0 +1,16 @@
+def add(n1,n2):
+    return(n1+n2)
+
+def sub(n1,n2):
+    return(n1-n2)
+
+def mul(n1,n2):
+    return(n1*n2)
+    
+
+a-int(input("enter num1:"))
+b=int(input("enter num2:"))
+
+print("sum=",add(a,b))
+print("sum=",sub(a,b))
+print("sum=",mul(a,b))
